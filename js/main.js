601de91909b4be620f5f3b894f6079f0114eb492
Dan Nguyen <dtn3423@gmail.com>
2017-09-15 11:57:10 -0400

    mobile response

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-/* JavaScript for portfolio */
+$(document).ready(function() {
+  navToggle();
+});
+
+function navToggle() {
+  $('.navicon').click(function() {
+    $('.navbar').addClass(' mobile');
+    $('.navbuttons').addClass(' mobile');
+    $('.navbuttons a').addClass(' showme');
+    $('.navicon').addClass(' hideme');
+    $('.navbar p').hide();
+    $('a').click(function() {
+      $('.navbar').removeClass(' mobile');
+      $('.navbuttons').removeClass(' mobile')
+      $('.navbuttons a').removeClass(' showme').addClass(' hideme');
+      $('.navicon')removeClass(' hideme').addClass(' showme');
+      $('.navbar p').show();
+      navToggle();
+    });
+  });
+};
+
+function checkSize() {
+  if ($(window).width() >= 400) {
+    $('.navbuttons a').show();
+    $('.navicon').hide();
+  }
+}
