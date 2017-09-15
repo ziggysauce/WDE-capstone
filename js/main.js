f61f5b9017fb65d333239042c74a07b117b61898
Dan Nguyen <dtn3423@gmail.com>
2017-09-15 12:29:01 -0400

    nav bar responsive

--- js/main.js
+++ js/main.js
@@ -7,22 +7,11 @@ function navToggle() {
     $('.navbar').addClass(' mobile');
     $('.navbuttons').addClass(' mobile');
     $('.navbuttons a').addClass(' showme');
-    $('.navicon').addClass(' hideme');
-    $('.navbar p').hide();
     $('a').click(function() {
       $('.navbar').removeClass(' mobile');
-      $('.navbuttons').removeClass(' mobile')
-      $('.navbuttons a').removeClass(' showme').addClass(' hideme');
-      $('.navicon')removeClass(' hideme').addClass(' showme');
-      $('.navbar p').show();
+      $('.navbuttons').removeClass(' mobile');
+      $('.navbuttons a').removeClass(' showme');
       navToggle();
     });
   });
 };
-
-function checkSize() {
-  if ($(window).width() >= 400) {
-    $('.navbuttons a').show();
-    $('.navicon').hide();
-  }
-}
